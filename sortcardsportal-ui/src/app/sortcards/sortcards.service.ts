eb7e238ca22d80c98a6adc4890231a49ce1a0f8d
Lenin-Venkat <vlenin74@gmail.com>
2022-04-03 22:49:32 +0530

    FinalCommitUI

--- sortcardsportal-ui/src/app/sortcards/sortcards.service.ts
+++ sortcardsportal-ui/src/app/sortcards/sortcards.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SortcardsService {
-private baseurl="https://localhost:5001/SortCardsController";
+// private baseurl="https://localhost:5001/SortCardsController";
+private baseurl="https://sortcardswebapp.azurewebsites.net/";
+//  baseurl = environment.baseUrl;
   constructor(private httpClient: HttpClient) { }
 
   sortCards(cardsList: string[]): Observable<string[]>
   {
     console.log(cardsList);
-    return this.httpClient.post<string[]>(this.baseurl+'', cardsList);
+    return this.httpClient.post<string[]>(this.baseurl+'SortCardsController', cardsList);
   }
 }
